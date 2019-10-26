317ab90e935759abd57e705450a669e90594a871
Dasaaf <as.f09942@gmail.com>
2019-10-25 22:16:41 -0500

    Se adiciona una prueba para registrar un usuario desde el frontend

--- horarios-api/controllers/userController.js
+++ horarios-api/controllers/userController.js
@@ -8,62 +8,87 @@ description:    El archivo userController contiene todos los métodos CRUD para 
 
 
 
-'use strict';//Activo el modo de uso estricto de javascript
+'use strict'; //Activo el modo de uso estricto de javascript
 
-var Usuario = require('../models/userModel');//Declaro un nuevo modelo de tipo Usuario
-var Conexion = require('./connectionController');//Declaro una variable conexión para conectarme a la base de datos MySQL
+var Usuario = require('../models/userModel'); //Declaro un nuevo modelo de tipo Usuario
+var Conexion = require('./connectionController'); //Declaro una variable conexión para conectarme a la base de datos MySQL
 
 //==============================>
 //======  CREATE USER  =========>
 //==============================>
-function createUser(req, res) {//Se crea una nueva función llamada createUser para registrar nuevos usuarios en el sistema
-    var params = req.body;//recupero los parametros de la petición que pidio el cliente (en éste caso la petición es crear un nuevo usuario)
-
+function createUser(req, res) { //Se crea una nueva función llamada createUser para registrar nuevos usuarios en el sistema
+    var params = req.body; //recupero los parametros de la petición que pidio el cliente (en éste caso la petición es crear un nuevo usuario)
     //GUARDANDO LOS DATOS DEL USUARIO QUE SERÁN GUARDADOS EN LA BASE DE DATOS
-    var usuario = new Usuario();//creo un nuevo modelo de usuario que va a estar vacío
-    usuario.correo = params.correo;//asigno al correo del modelo usuario vacío el dato del correo que envío el cliente 
-    usuario.nombres = params.nombres;//asigno al nombre del modelo usuario vacío el dato del nombre que envío el cliente 
-    usuario.apellidos = params.apellidos;//asigno a los apellidos del modelo usuario vacío el dato de los apellidos que envío el cliente 
-    usuario.clave = params.clave;//asigno a la clave del modelo usuario vacío el dato de la clave que envío el cliente 
+    var usuario = new Usuario(); //creo un nuevo modelo de usuario que va a estar vacío
+    usuario.correo = params.correo; //asigno al correo del modelo usuario vacío el dato del correo que envío el cliente 
+    usuario.nombres = params.nombres; //asigno al nombre del modelo usuario vacío el dato del nombre que envío el cliente 
+    usuario.apellidos = params.apellidos; //asigno a los apellidos del modelo usuario vacío el dato de los apellidos que envío el cliente 
+    usuario.clave = params.clave; //asigno a la clave del modelo usuario vacío el dato de la clave que envío el cliente 
 
     //INSERTAR DATO EN LA BASE DE DATOS------>
-    var sql = "INSERT INTO usuarios(nombres, apellidos, correo, clave) VALUES ('" + usuario.nombres + "', '" + usuario.apellidos + "', '" + usuario.correo + "','" + usuario.clave + "')";//declaro una variable que almacenara la petición a la base de datos, en éste caso INSERT INTO para CREAR un nuevo usuario
-    Conexion.CONNECTION.query(sql, function (error, result) {//con la variable conexion creada anteriormente accedo a la conexión para hacer una nueva consulta de tipo SQL y la ejecuto
-        if (error) throw error;//Si hay algún error, muestro el error
+    var sql = "INSERT INTO usuarios(nombres, apellidos, correo, clave) VALUES ('" + usuario.nombres + "', '" + usuario.apellidos + "', '" + usuario.correo + "','" + usuario.clave + "')"; //declaro una variable que almacenara la petición a la base de datos, en éste caso INSERT INTO para CREAR un nuevo usuario
+    Conexion.CONNECTION.query(sql, function (error, result) { //con la variable conexion creada anteriormente accedo a la conexión para hacer una nueva consulta de tipo SQL y la ejecuto
+        if (error) throw error; //Si hay algún error, muestro el error
 
         //RESPUESTA DEL SERVIDOR CUANDO SE CREA UN USUARIO------>
-        res.status(200).send({//Si no hay error preparo una nueva RESPUESTA con estado=200 que significa que todo salio bien
-            message: 'Usuario creado',//en la RESPUESTA que será enviada adjunto un mensaje que dice "Usuario creado"
-            result: result,//también adiciona a la RESPUESTA el resultado al insertar el registro en la base de datos
-            error: error//Y por último, si existe algún error también lo envío en la RESPUESTA
+        res.status(200).send({ //Si no hay error preparo una nueva RESPUESTA con estado=200 que significa que todo salio bien
+            message: 'Usuario creado', //en la RESPUESTA que será enviada adjunto un mensaje que dice "Usuario creado"
+            result: result, //también adiciona a la RESPUESTA el resultado al insertar el registro en la base de datos
+            error: error //Y por último, si existe algún error también lo envío en la RESPUESTA
         });
 
-    }
-    );
+    });
 }
+
+
+
+//==============================>
+//======  USER LOGIN  ==========>
+//==============================>
+function login(req, res) {
+    var params = req.body;
+    var sql = "SELECT * from usuarios where correo = '" + params.correo + "' AND clave='" + params.clave + "'";
+    var message = "";
+    Conexion.CONNECTION.query(sql, function (error, result) {
+        if (error) throw error;
+        if (result.length!=0) {
+            message = "Usuario y clave son correctos";
+        } else {
+            message = "No se ha encontrado el usuario o la clave no coincide";
+        }
+        res.status(200).send({
+            message: message,
+            result: result,
+            error: error
+        });
+
+    });
+}
+
 
 
 //==============================>
 //======  READ USERS  ==========>
 //==============================>
-function readUsers(req, res) {//Se crea una nueva función llamada readUsers que servirá para consultar todos los usuarios registrados en la base de datos
-    var sql = "SELECT * from usuarios";//Se declara una nueva variable llamada sql la cual almacenará la consulta a la base de datos
-    Conexion.CONNECTION.query(sql, function (error, result) {////con la variable conexion creada anteriormente accedo a la conexión para hacer una nueva consulta de tipo SQL y la ejecuto
-        if (error) throw error;//Si hay algún error, muestro el error
+function readUsers(req, res) { //Se crea una nueva función llamada readUsers que servirá para consultar todos los usuarios registrados en la base de datos
+    var sql = "SELECT * from usuarios"; //Se declara una nueva variable llamada sql la cual almacenará la consulta a la base de datos
+    Conexion.CONNECTION.query(sql, function (error, result) { ////con la variable conexion creada anteriormente accedo a la conexión para hacer una nueva consulta de tipo SQL y la ejecuto
+        if (error) throw error; //Si hay algún error, muestro el error
 
         //RESPUESTA DEL SERVIDOR CUANDO SE CREA UN USUARIO------>
-        res.status(200).send({//Si no hay error preparo una nueva RESPUESTA con estado=200 que significa que todo salio bien
-            users: result,//Adicionó a la RESPUESTA el resultado de la consulta a la base de datos
-            error: error//Adiciono a la RESPUESTA el error en caso de que exista un error
+        res.status(200).send({ //Si no hay error preparo una nueva RESPUESTA con estado=200 que significa que todo salio bien
+            users: result, //Adicionó a la RESPUESTA el resultado de la consulta a la base de datos
+            error: error //Adiciono a la RESPUESTA el error en caso de que exista un error
         });
     });
 }
 
 
+
 //==============================>
 //======  READ USER  ==========>
 //==============================>
-function readUser(req, res) {
+function readUser(req, res) { //Se crea una nueva función llamada readUser que servirá para consultar un usuario de la base de datos
     var userId = req.params.user_id;
     var sql = "SELECT * from usuarios where id = " + userId;
     Conexion.CONNECTION.query(sql, function (error, result) {
@@ -76,10 +101,11 @@ function readUser(req, res) {
 }
 
 
+
 //==============================>
 //======  UPDATE USER  =========>
 //==============================>
-function updateUser(req, res) {
+function updateUser(req, res) { //Se crea una nueva función llamada updateUser que servirá para actualizar los datos de un usuario
     var userId = req.params.user_id;
     var params = req.body;
 
@@ -99,10 +125,11 @@ function updateUser(req, res) {
 }
 
 
+
 //==============================>
 //======  DELETE USER  =========>
 //==============================>
-function deleteUser(req, res) {
+function deleteUser(req, res) { //Se crea un nueva función llamada deleteUser que servirá para eliminar un usuario
     var userId = req.params.user_id;
     var sql = "DELETE FROM usuarios WHERE id = " + userId;
 
@@ -114,8 +141,12 @@ function deleteUser(req, res) {
         });
     });
 }
+
+
+
 module.exports = {
     createUser,
+    login,
     readUsers,
     readUser,
     updateUser,
--- horarios-api/routes/userRoutes.js
+++ horarios-api/routes/userRoutes.js
@@ -8,16 +8,17 @@ description:    El archivo userRoutes contiene todas las rutas con las cuales el
 
 
 
-var express = require('express');//Se crea una nueva variable llamada express y se le asigna el módulo de express
-var UserController = require('../controllers/userController');//Se crea una nueva variable llamada UserController y se le asigna el archivo userControllers que es dónde están los métodos CRUD
-var api = express.Router();//Se crea una variable llamada api a la cuál guardamos el método Router() que está en el módulo de express
+var express = require('express'); //Se crea una nueva variable llamada express y se le asigna el módulo de express
+var UserController = require('../controllers/userController'); //Se crea una nueva variable llamada UserController y se le asigna el archivo userControllers que es dónde están los métodos CRUD
+var api = express.Router(); //Se crea una variable llamada api a la cuál guardamos el método Router() que está en el módulo de express
 
 
-api.post('/user', UserController.createUser);//a la variable api, adicionamos una nueva ruta de tipo POST que sirve para crear un nuevo usuario
-api.get('/users', UserController.readUsers);//a la variable api, adicionamos una nueva ruta de tipo GET que sirve para listar todos los usuarios
-api.get('/user/:user_id', UserController.readUser);//a la variable api, adicionamos una nueva ruta de tipo GET que sirve para ver un sólo usuario dado el id de ese usuario que desea ver
-api.put('/user/:user_id', UserController.updateUser);//a la variable api, adicionamos una nueva ruta de tipo PUT que sirve para actualizar los datos de un usuario dado el id del usuario que deseo actualizar
-api.delete('/user/:user_id', UserController.deleteUser);//a la variable api, adicionamos una nueva ruta de tipo DELETE que sirve para eliminar un usuario dado el id del usuario que se desea eliminar
+api.post('/user', UserController.createUser); //a la variable api, adicionamos una nueva ruta de tipo POST que sirve para crear un nuevo usuario
+api.post('/login', UserController.login); //a la variable api, adicionamos una nueva ruta de tipo POST que sirve para verificar que el correo y la contraseña si sean correctos
+api.get('/users', UserController.readUsers); //a la variable api, adicionamos una nueva ruta de tipo GET que sirve para listar todos los usuarios
+api.get('/user/:user_id', UserController.readUser); //a la variable api, adicionamos una nueva ruta de tipo GET que sirve para ver un sólo usuario dado el id de ese usuario que desea ver
+api.put('/user/:user_id', UserController.updateUser); //a la variable api, adicionamos una nueva ruta de tipo PUT que sirve para actualizar los datos de un usuario dado el id del usuario que deseo actualizar
+api.delete('/user/:user_id', UserController.deleteUser); //a la variable api, adicionamos una nueva ruta de tipo DELETE que sirve para eliminar un usuario dado el id del usuario que se desea eliminar
 
 
-module.exports = api;//exportamos todas las rutas para que el cliente pueda acceder a ellas
+module.exports = api; //exportamos todas las rutas para que el cliente pueda acceder a ellas
